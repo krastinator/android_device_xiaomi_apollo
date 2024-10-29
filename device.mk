a2507c5d7225f65fbead49b21dc6d25c1f39e97e
johnmart19 <johnivan19999@gmail.com>
2024-10-29 18:35:08 +0200

    apollo: Import Dolby Props

--- device.mk
+++ device.mk
@@ -151,6 +151,13 @@ TARGET_BUILD_DEVICE_AS_WEBCAM := true
 PRODUCT_PACKAGES += \
     XiaomiParts
 
+# Dolby Props
+PRODUCT_VENDOR_PROPERTIES += \
+    ro.vendor.dolby.dax.version=DAX3_3.6.1.6_r1 \
+    ro.vendor.audio.dolby.dax.version=DAX3_3.6 \
+    ro.vendor.audio.dolby.dax.support=true \
+    ro.vendor.audio.dolby.surround.enable=true
+
 # Display
 PRODUCT_PACKAGES += \
     vendor.qti.hardware.display.composer-service
