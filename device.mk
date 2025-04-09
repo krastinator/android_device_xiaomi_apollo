49cb894bacd7f4979779b0b8539d43f0a7d39e09
Peter Meiser <peter.meiser@gmx.com>
2025-04-09 06:56:37 +0200

    apollo: build DeviceAsWebcam service

--- device.mk
+++ device.mk
@@ -144,6 +144,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.ir-service.lineage
 
+# DeviceAsWebcam
+TARGET_BUILD_DEVICE_AS_WEBCAM := true
+
 # Device-specific settings
 PRODUCT_PACKAGES += \
     XiaomiParts
