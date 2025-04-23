83fbdaa609b6d4673ccce2ea8f4a0eb286930af7
redznn <redznn@protonmail.com>
2025-04-23 00:18:01 +0000

    apollo: Build libutils E vndksupport: Could not load libutils.so from sphal namespace: dlopen failed: library "libutils.so" not found.

--- device.mk
+++ device.mk
@@ -341,6 +341,10 @@ PRODUCT_PACKAGES += \
 # Vibrator
 $(call inherit-product, vendor/qcom/opensource/vibrator/vibrator-vendor-product.mk)
 
+# VNDK
+PRODUCT_PACKAGES += \
+    libutils.vendor
+
 # Wi-Fi
 PRODUCT_PACKAGES += \
     android.hardware.wifi-service \
