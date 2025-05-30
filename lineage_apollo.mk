f9f40c123d5f5c0c1b2fdcb3b7437a8e24d2de9f
Murtaza <murtazakolachi7@gmail.com>
2025-05-30 18:25:10 +0500

    apollo: Axion bringup

--- lineage_apollo.mk
+++ lineage_apollo.mk
@@ -14,6 +14,31 @@ $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 # Inherit from apollo device
 $(call inherit-product, device/xiaomi/apollo/device.mk)
 
+#Axion Bringup!
+TARGET_INCLUDES_LOS_PREBUILTS := true
+PRODUCT_NO_CAMERA := false
+TARGET_INCLUDE_VIPERFX := true
+
+AXION_CAMERA_REAR_INFO := 64,13,5
+
+AXION_CAMERA_FRONT_INFO := 20
+
+AXION_MAINTAINER := Murtaza_Kolachi
+
+AXION_PROCESSOR := Snapdragon_865_(7nm)
+
+AXION_CPU_PRIME_CORES := 7
+AXION_CPU_BIG_CORES := 4,5,6
+AXION_CPU_SMALL_CORES := 0,1,2,3
+
+PRODUCT_SYSTEM_PROPERTIES += \
+    persist.sys.axion_cpu_prime=$(AXION_CPU_PRIME_CORES) \
+    persist.sys.axion_cpu_big=$(AXION_CPU_BIG_CORES) \
+    persist.sys.axion_cpu_small=$(AXION_CPU_SMALL_CORES)
+
+# Boot animation
+TARGET_BOOT_ANIMATION_RES := 1080
+
 PRODUCT_NAME := lineage_apollo
 PRODUCT_DEVICE := apollo
 PRODUCT_MANUFACTURER := Xiaomi
